d4bda7b7453916c6a54947a2b273b747d9325945
Ko Htutz <info@kohtut.me>
2022-03-10 12:24:52 +0630

    New : Firebase Messaging & Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,94 @@
 import 'dart:async';
+import 'dart:math';
 
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:vpn/vpnApp.dart';
 
 void main() async {
-  await runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-    runApp(const VPNApp());
-  }, (error, stackTrace) {
-    FirebaseCrashlytics.instance.recordError(error, stackTrace);
-  });
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      FirebaseMessaging.instance.requestPermission();
+      AwesomeNotifications().initialize(
+          // set the icon to null if you want to use the default app icon
+          'resource://drawable/ic_launcher',
+          [
+            NotificationChannel(
+                channelGroupKey: 'ovo_vpn_group',
+                channelKey: 'ovo_vpn_group',
+                channelName: 'OvO VPN',
+                channelDescription: 'Hey My Friend',
+                defaultColor: const Color(0xFF2861FF),
+                ledColor: Colors.white)
+          ],
+          // Channel groups are only visual and are not required
+          channelGroups: [
+            NotificationChannelGroup(
+                channelGroupkey: 'ovo_vpn_group', channelGroupName: 'Vpn group')
+          ],
+          debug: true);
+
+      FirebaseMessaging.onBackgroundMessage(
+          _firebaseMessagingBackgroundHandler);
+      await FirebaseMessaging.instance
+          .setForegroundNotificationPresentationOptions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+      runApp(
+        const VPNApp(),
+      );
+    },
+    (error, stackTrace) {
+      FirebaseCrashlytics.instance.recordError(error, stackTrace);
+    },
+  );
 }
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.messageId}');
+
+  if (!AwesomeStringUtils.isNullOrEmpty(message.notification?.title,
+          considerWhiteSpaceAsEmpty: true) ||
+      !AwesomeStringUtils.isNullOrEmpty(message.notification?.body,
+          considerWhiteSpaceAsEmpty: true)) {
+    print('message also contained a notification: ${message.notification}');
+
+    String? imageUrl;
+    imageUrl ??= message.notification!.android?.imageUrl;
+    imageUrl ??= message.notification!.apple?.imageUrl;
+
+    Map<String, dynamic> notificationAdapter = {
+      NOTIFICATION_CHANNEL_KEY: 'ovo_vpn_group',
+      NOTIFICATION_ID: message.data[NOTIFICATION_CONTENT]?[NOTIFICATION_ID] ??
+          message.messageId ??
+          Random().nextInt(2147483647),
+      NOTIFICATION_TITLE: message.data[NOTIFICATION_CONTENT]
+              ?[NOTIFICATION_TITLE] ??
+          message.notification?.title,
+      NOTIFICATION_BODY: message.data[NOTIFICATION_CONTENT]
+              ?[NOTIFICATION_BODY] ??
+          message.notification?.body,
+      NOTIFICATION_LAYOUT:
+          AwesomeStringUtils.isNullOrEmpty(imageUrl) ? 'Default' : 'BigPicture',
+      NOTIFICATION_BIG_PICTURE: imageUrl
+    };
+
+    AwesomeNotifications().createNotificationFromJsonData(notificationAdapter);
+  } else {
+    AwesomeNotifications().createNotificationFromJsonData(message.data);
+  }
+}
 
 // fvm flutter run | grep -v "Error retrieving thread information"
 // Fvm build runner gen
--- lib/ui/splash_ui.dart
+++ lib/ui/splash_ui.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vpn/Router/route.dart';
@@ -15,11 +16,28 @@ class _SplashUIState extends State<SplashUI> {
   void initState() {
     // ignore: todo
     // TODO: implement initState
+    noti();
+
+    super.initState();
+  }
+
+  void noti() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        // This is just a basic example. For real apps, you must show some
+        // friendly dialog box before call the request method.
+        // This is very important to not harm the user experience
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+    nextPage();
+  }
+
+  void nextPage() {
     Timer(const Duration(seconds: 2), () {
       // Get.toNamed(VPNRoute.home);
       Get.offAllNamed(VPNRoute.home);
     });
-    super.initState();
   }
 
   @override
